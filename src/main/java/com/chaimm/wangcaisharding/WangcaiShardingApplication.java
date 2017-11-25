31c3f40850b1e47c1b587a3503c1d9d02b8604fc
chaibozhou <350142639@qq.com>
2017-11-25 10:30:49 +0800

    init commit

---
+++ src/main/java/com/chaimm/wangcaisharding/WangcaiShardingApplication.java
@@ -0,0 +1,12 @@
+package com.chaimm.wangcaisharding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WangcaiShardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WangcaiShardingApplication.class, args);
+	}
+}
